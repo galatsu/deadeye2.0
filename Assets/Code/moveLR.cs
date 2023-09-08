e0c76f179f6cb2dfb746d37f0c45198c2269e94c
galatsu <tracy4fun.minecraft@gmail.com>
2023-09-08 13:13:36 -0400

    player movement script added
    onto temp sprite

--- Assets/Code/moveLR.cs
+++ Assets/Code/moveLR.cs
@@ -1,9 +1,13 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class moveLR : MonoBehaviour
 {
+
+    public float playerSpeed = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class moveLR : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPos = transform.position;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            newPos.x -= playerSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPos.x += playerSpeed * Time.deltaTime;
+        }
+
+        transform.position = newPos;
     }
 }
