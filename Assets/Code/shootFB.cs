f7b7b5a778523b34a81060662e4d7d05e568715a
galatsu <tracy4fun.minecraft@gmail.com>
2023-09-10 00:48:15 -0400

    pt 2 of previous push
    still trying to figure it out

--- Assets/Code/shootFB.cs
+++ Assets/Code/shootFB.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class shootFB : MonoBehaviour
 {
+    public int fireballSpeed;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(0, fireballSpeed);
     }
 }
