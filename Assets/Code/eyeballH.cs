ed548c692faa9edb206aa7722f2f3e70f62a6772
galatsu <tracy4fun.minecraft@gmail.com>
2023-09-12 19:30:02 -0400

    eyeball now bounces off of walls and ceiling!

--- Assets/Code/eyeballH.cs
+++ Assets/Code/eyeballH.cs
@@ -7,24 +7,46 @@ public class eyeballH : MonoBehaviour
 {
     //public Sprite eyeballHit;
     //public Sprite eyeball;
-    public AudioSource audio;
-    //public SpriteRenderer render;
+    public float push = 10;
+    Rigidbody2D rb;
+    public AudioSource audio1;
+    public AudioSource audio2;
     public string failstate;
-    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
 
         if (collision.gameObject.tag == "fireball")
         {
             //Debug.Log("hit");
-            audio.Play();
+            audio1.Play();
             //render.sprite = eyeballHit;
 
-        } else if (collision.gameObject.tag == "ceiling" || collision.gameObject.tag == "wall")
+        }
+        else if (collision.gameObject.tag == "ceiling")
         {
             Debug.Log("hit");
+            rb.AddForce(Vector2.down * push, ForceMode2D.Impulse);
+            audio2.Play();
 
-        } else if (collision.gameObject.tag == "floor")
+        }
+        else if (collision.gameObject.tag == "wall")
+        {
+            rb.AddForce(Vector2.left * push, ForceMode2D.Impulse);
+            audio2.Play();
+
+        }
+        else if (collision.gameObject.tag == "otherWall")
+        {
+            rb.AddForce(Vector2.right * push, ForceMode2D.Impulse);
+            audio2.Play();
+        }
+        else if (collision.gameObject.tag == "floor")
         {
             SceneManager.LoadScene(failstate);
         }
